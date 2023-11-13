911add050828615d27cfb4ec71b3c1bb01743f07
BaderTim <bader.timalexander@gmail.com>
2023-11-13 19:40:49 +0100

    introduced wrapper for 2d and 3d scans

---
+++ src/main/java/io/github/mmm/measurement/utils/Scan.java
@@ -0,0 +1,40 @@
+package io.github.mmm.measurement.utils;
+
+public class Scan {
+
+    private Scan2D scan2D;
+    private Scan3D scan3D;
+    private boolean is2D = false;
+
+    public Scan(Scan2D scan2D) {
+        this.scan2D = scan2D;
+        is2D = true;
+    }
+
+    public Scan(Scan3D scan3D) {
+        this.scan3D = scan3D;
+        this.is2D = false;
+    }
+
+    @Override
+    public String toString() {
+        if (is2D) {
+            return scan2D.toString();
+        } else {
+            return scan3D.toString();
+        }
+    }
+
+    public Scan2D getScan2D() {
+        return scan2D;
+    }
+
+    public Scan3D getScan3D() {
+        return scan3D;
+    }
+
+    public boolean is2D() {
+        return is2D;
+    }
+
+}
