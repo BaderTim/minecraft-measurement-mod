e235fe660836f642dbbec404b69a998129676d07
BaderTim <bader.timalexander@gmail.com>
2023-11-13 18:27:42 +0100

    added tickrate observer

---
+++ src/main/java/io/github/mmm/measurement/TickObserver.java
@@ -0,0 +1,44 @@
+package io.github.mmm.measurement;
+
+import io.github.mmm.MMM;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import static io.github.mmm.MMM.MEASUREMENT_MANAGER;
+import static io.github.mmm.MMM.MODID;
+
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class TickObserver {
+
+    public static final int TOLERANCE_IN_MS = 5;
+    public static final int TICKS_PER_SECOND = 20;
+    public static int messageTimeoutInTicks = TICKS_PER_SECOND*5; // 5 seconds
+    public static long lastTickTime = 0;
+    public static int tickCount = 0;
+
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        Player player = Minecraft.getInstance().player;
+        if (event.phase == TickEvent.Phase.END && player != null) {
+            long currentTime = System.currentTimeMillis();
+            if (lastTickTime == 0) {
+                lastTickTime = currentTime;
+                return;
+            }
+            if (MEASUREMENT_MANAGER.isCurrentlyMeasuring() && (currentTime - lastTickTime - 1000/TICKS_PER_SECOND - TOLERANCE_IN_MS) > 0) {
+                if (tickCount > messageTimeoutInTicks) {
+                    player.displayClientMessage(Component.translatable("chat." + MMM.MODID + ".warning.tickrate"), false);
+                    tickCount = 0;
+                }
+            }
+            if(tickCount > 1000000) tickCount = messageTimeoutInTicks; // avoid overflow
+            tickCount++;
+            lastTickTime = currentTime;
+        }
+    }
+}
